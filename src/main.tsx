9fbe8e141cb5bb948d0dd3ce1cc249d763284980
MallenK <sergimallenweb@gmail.com>
2026-04-16 12:58:23 +0200

    modificar url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/WebJPPreparation3/">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </StrictMode>
