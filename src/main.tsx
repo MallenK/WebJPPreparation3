8728bcb079d6feccfb0d824b0447516758f93e1b
MallenK <sergimallenweb@gmail.com>
2026-04-16 13:24:12 +0200

    recuperar url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/WebJPPreparation3/">
       <App />
     </BrowserRouter>
   </StrictMode>
